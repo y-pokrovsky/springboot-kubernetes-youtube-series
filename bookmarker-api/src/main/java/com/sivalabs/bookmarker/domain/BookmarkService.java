5007d29f983391e573d2cdf9a5d3f6b9db18f54b
Yura Pokrovskiy <y-pokrovsky@yandex.ru>
2026-05-14 19:41:36 +0300

    initial commit

---
+++ bookmarker-api/src/main/java/com/sivalabs/bookmarker/domain/BookmarkService.java
@@ -0,0 +1,25 @@
+package com.sivalabs.bookmarker.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BookmarkService {
+    private final BookmarkRepository repository;
+    private final BookmarkMapper bookmarkMapper;
+
+    @Transactional(readOnly = true)
+    public BookmarksDTO getBookmarks(Integer page) {
+        int pageNo = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
+        Page<BookmarkDTO> bookmarkPage = repository.findBy(pageable);
+        return new BookmarksDTO(bookmarkPage);
+    }
+}
---
+++ bookmarker-api/src/main/java/com/sivalabs/bookmarker/domain/BookmarksDTO.java
@@ -0,0 +1,94 @@
+package com.sivalabs.bookmarker.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public class BookmarksDTO {
+    private List<BookmarkDTO> data;
+    private long totalElements;
+    private int totalPages;
+    private int currentPage;
+    private boolean isFirst;
+    private boolean isLast;
+    private boolean hasNext;
+    private boolean hasPrevious;
+
+    public BookmarksDTO(Page<BookmarkDTO> bookmarkPage) {
+        this.setData(bookmarkPage.getContent());
+        this.setTotalElements(bookmarkPage.getTotalElements());
+        this.setTotalPages(bookmarkPage.getTotalPages());
+        this.setCurrentPage(bookmarkPage.getNumber() + 1);
+        this.setFirst(bookmarkPage.isFirst());
+        this.setLast(bookmarkPage.isLast());
+        this.setHasNext(bookmarkPage.hasNext());
+        this.setHasPrevious(bookmarkPage.hasPrevious());
+    }
+
+    public List<BookmarkDTO> getData() {
+        return data;
+    }
+
+    public void setData(List<BookmarkDTO> data) {
+        this.data = data;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public boolean isFirst() {
+        return isFirst;
+    }
+
+    @JsonProperty("isFirst")
+    public void setFirst(boolean first) {
+        isFirst = first;
+    }
+
+    public boolean isLast() {
+        return isLast;
+    }
+
+    @JsonProperty("isLast")
+    public void setLast(boolean last) {
+        isLast = last;
+    }
+
+    public boolean isHasNext() {
+        return hasNext;
+    }
+
+    public void setHasNext(boolean hasNext) {
+        this.hasNext = hasNext;
+    }
+
+    public boolean isHasPrevious() {
+        return hasPrevious;
+    }
+
+    public void setHasPrevious(boolean hasPrevious) {
+        this.hasPrevious = hasPrevious;
+    }
+}
