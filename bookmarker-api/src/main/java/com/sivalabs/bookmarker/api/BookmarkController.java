a83a627dec78ae4bdffd91ac15c56981c94eb4c3
Yura Pokrovskiy <y-pokrovsky@yandex.ru>
2026-05-15 19:53:24 +0300

    implementing search bookmarks

--- bookmarker-api/src/main/java/com/sivalabs/bookmarker/api/BookmarkController.java
+++ bookmarker-api/src/main/java/com/sivalabs/bookmarker/api/BookmarkController.java
@@ -1,15 +1,9 @@
 package com.sivalabs.bookmarker.api;
 
-import com.sivalabs.bookmarker.domain.Bookmark;
 import com.sivalabs.bookmarker.domain.BookmarkService;
 import com.sivalabs.bookmarker.domain.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/bookmarks")
@@ -19,8 +13,12 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
-        return bookmarkService.getBookmarks(page);
+    public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page,
+                                     @RequestParam(name = "query", defaultValue = "") String query) {
+        if (query == null || query.trim().length() == 0) {
+            return bookmarkService.getBookmarks(page);
+        }
+        return bookmarkService.searchBookmarks(query, page);
     }
 
 }
--- bookmarker-api/src/main/java/com/sivalabs/bookmarker/domain/Bookmark.java
+++ bookmarker-api/src/main/java/com/sivalabs/bookmarker/domain/Bookmark.java
@@ -24,5 +24,4 @@ public class Bookmark {
     @Column(nullable = false)
     private String url;
     private Instant createdAt;
-
 }
